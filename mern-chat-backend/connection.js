32c19b104815b0d023290c678ba420cced8d2daa
Hanan <hananjohn111@gmail.com>
2023-06-26 18:37:35 +0500

    Connected to DB

--- mern-chat-backend/connection.js
+++ mern-chat-backend/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://yarooq1:uYmAHPXUovjWkqwX@cluster0.wjm3szj.mongodb.net/?retryWrites=true&w=majority`, ()=> {
+mongoose.connect(`mongodb+srv://mernchat-yt-user:5Qp3w6P8QCX0RyF9@cluster0.rgfwyzs.mongodb.net/?retryWrites=true&w=majority`, ()=> {
   console.log('connected to mongodb')
 })
